0e0d8a766d248b8f0c00a20bdc06b8b86656b321
Nabil BOROUS <nabil.borous@supdevinci-edu.fr>
2026-04-08 12:07:54 +0200

    protection main

--- db_init.py
+++ db_init.py
@@ -1,16 +1,16 @@
 import pymongo
 from models import *
-
-# Création de la base de donnée
 client = pymongo.MongoClient("mongodb://localhost:27017")
 Database = client["Database"]
 CharacterCol = Database["Personnages"]
 MonsterCol = Database["Monstres"]
 ScoreCol = Database["Scores"]
 
-CharacterCol.drop()
-MonsterCol.drop()
-
-# Insertion dans la base de donnée
-CharacterCol.insert_one(dictPersonnages)
-MonsterCol.insert_one(dictMonstres)
+if __name__ == 'main':
+    # Création de la base de donnée
+    CharacterCol.drop()
+    MonsterCol.drop()
+    
+    # Insertion dans la base de donnée
+    CharacterCol.insert_one(dictPersonnages)
+    MonsterCol.insert_one(dictMonstres)
